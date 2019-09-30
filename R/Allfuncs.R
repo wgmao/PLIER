4e5594643da35dfffd5d6a11426790dd46042992
wgmao <mwg10.thu@gmail.com>
2019-09-30 15:56:24 -0400

    Update Allfuncs.R

--- R/Allfuncs.R
+++ R/Allfuncs.R
@@ -39,6 +39,7 @@ solveU=function(Z,  Chat, priorMat, penalty.factor,pathwaySelection="fast", glm_
       gres=glmnet(y=Z[,i], x=priorMat[,iip], penalty.factor = penalty.factor[iip], alpha=glm_alpha, lower.limits=0, lambda = lambdas,intercept=T,  standardize=F )
       #plot(gres)
       gres$iip=iip
+      print(gres$beta)
       lMat[,i]=colSums(gres$beta>0)
       results[[i]]=gres
     }
